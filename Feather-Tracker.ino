ea4dc4a8f187a7137dd084edaff76990a1b9cc44
Don Zheng ⚡️ <zhengdo@amazon.com>
2020-09-16 22:47:31 -0700

    Added debug print and default values for GPS

--- Feather-Tracker.ino
+++ Feather-Tracker.ino
@@ -2,7 +2,16 @@
 #include <Adafruit_GPS.h>
 #include <SPI.h>
 
+#define DEBUG
 #define GPSSerial Serial1
+
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)  Serial.println (x)
+ #define DEBUG_LED(x) digitalWrite(LED_BUILTIN, x)
+#else
+ #define DEBUG_PRINT(x)
+ #define DEBUG_LED(x)
+#endif
 
 // Network Session Key (MSB)
 uint8_t NwkSkey[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
@@ -16,20 +25,21 @@ uint8_t DevAddr[4] = { 0x00, 0x00, 0x00, 0x00 };
 // How many times data transfer should occur, in seconds
 const unsigned int sendInterval = 30;
 
-// Pinout for Adafruit Feather 32u4 LoRa
 TinyLoRa lora = TinyLoRa(7, 8, 4);
 Adafruit_GPS GPS(&GPSSerial);
 
-unsigned char loraData[10];
+unsigned char loraData[15];
+float latitude;
+float longitude;
 
 void setupLora() {
-  Serial.print("Starting LoRa...");
+  DEBUG_PRINT("Starting LoRa...");
   lora.setChannel(MULTI);
   lora.setDatarate(SF7BW125);
   if(!lora.begin())
   {
-    Serial.println("LoRa connection Failed");
-    Serial.println("Check your radio");
+    DEBUG_PRINT("LoRa connection Failed");
+    DEBUG_PRINT("Check your radio");
     while(true) {
         digitalWrite(LED_BUILTIN, HIGH);
         delay(250);
@@ -38,30 +48,43 @@ void setupLora() {
     }
   }
 
-  Serial.println("LoRa Setup Complete");
+  DEBUG_PRINT("LoRa Setup Complete");
 }
 
 void setupGPS() {
+  DEBUG_PRINT("Setting up GPS...");
   GPS.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ); 
   GPS.sendCommand(PMTK_API_SET_FIX_CTL_100_MILLIHERTZ); 
 
   delay(1000);
-  Serial.println("GPS Setup Complete");  
+  DEBUG_PRINT("GPS Setup Complete");  
 }
 
 void setCoords() {
+  if (GPS.fix) {
+    latitude = GPS.latitude;
+    longitude = GPS.longitude;
+  } else {
+    latitude = 0.0;
+    longitude = 0.0;
+  }
+  
+  DEBUG_PRINT(printf("Latitude: %.4f, Longitude: %.4f", latitude, longitude));
   snprintf((char*)loraData, sizeof(loraData), "%.4f%c%.4f%c", 
    GPS.latitude, GPS.lat, GPS.longitude, GPS.lon);
 }
 
 void sendCoords() {
+  DEBUG_PRINT("Sending LoRa Data..."); 
+  DEBUG_PRINT((char*)loraData);
   lora.sendData(loraData, sizeof(loraData), lora.frameCounter);
+  DEBUG_PRINT("Data sent successfully");
   lora.frameCounter++;
-  digitalWrite(LED_BUILTIN, HIGH);
+  DEBUG_LED(HIGH);
   delay(250);
-  digitalWrite(LED_BUILTIN, LOW);
+  DEBUG_LED(LOW);
 }
 
 void setup()
@@ -79,7 +102,6 @@ void setup()
 void loop()
 {
   setCoords();
-  Serial.println("Sending LoRa Data..."); Serial.print(loraData);
   sendCoords();
   
   delay(sendInterval * 1000);
